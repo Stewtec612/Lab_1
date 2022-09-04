a50b6f7e50c448cf8d283c188b6ed72602875108
Stewart Christie <stewart.chrst99@gmail.com>
2022-09-04 11:37:24 -0500

    main function inserted

---
+++ Lab1/lab_1_camel_case/lab_1_camel_case.py
@@ -0,0 +1,37 @@
+'''
+Convert any sentence entered as camel case
+The first word is not capitalized 
+and the other words are capitalized and no spaces in-between
+
+'''
+
+from re import sub
+
+
+
+
+
+#function converts the user's input into a camelCase format
+def camilize(userInput):
+
+    
+    userInput = sub(r"(_|-)+"," ", userInput).title().replace(" ", "")
+    return ''.join([userInput[0].lower(), userInput[1:]])
+
+def main():
+
+    print('Convert any sentence into camelCase')
+
+    userInput = input('Enter a sentence: ')
+
+    print(camilize(userInput))
+
+main()
+
+
+
+
+
+
+
+
