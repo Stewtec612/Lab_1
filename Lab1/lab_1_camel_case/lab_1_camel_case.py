66e5a374d63235fdbc862e1e65622b83fc0bd417
Stewart Christie <stewart.chrst99@gmail.com>
2022-09-04 11:32:21 -0500

    Add banner welcome message

---
+++ Lab1/lab_1_camel_case/lab_1_camel_case.py
@@ -0,0 +1,42 @@
+'''
+Convert any sentence entered as camel case
+The first word is not capitalized 
+and the other words are capitalized and no spaces in-between
+
+'''
+
+from re import sub
+
+
+
+def banner():
+    """Display program name"""
+    message = 'Awesome camel case program!'
+    stars = '*' * len(message)
+    print(f'\n{stars} \n{message} \n{stars}\n')
+
+#function converts the user's input into a camelCase format
+def camilize(userInput):
+
+    
+    userInput = sub(r"(_|-)+"," ", userInput).title().replace(" ", "")
+    return ''.join([userInput[0].lower(), userInput[1:]])
+
+def main():
+    banner()
+
+    print('Convert any sentence into camelCase')
+
+    userInput = input('Enter a sentence: ')
+
+    print(camilize(userInput))
+
+main()
+
+
+
+
+
+
+
+
